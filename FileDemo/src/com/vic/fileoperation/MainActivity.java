187385247c2e6d74c08b87619dd62d4cae3654e3
vichw <vic_ch@126.com>
2013-07-27 15:41:44 +0800

    Update MainActivity.java
    test

--- FileDemo/src/com/vic/fileoperation/MainActivity.java
+++ FileDemo/src/com/vic/fileoperation/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        System.out.println("");
+        System.out.println("test");
         
         
         
